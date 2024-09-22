aad077fb68c29c184998ac661fc6b7c5e28f6fb7
WictorPamp <wictorpamplona98@gmail.com>
2024-09-22 15:52:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## GarçomBot &nbsp; 
+
+Este aplicativo terá como principal ferramenta gerenciar pedidos tanto fisícos quanto digitais de restaurantes, bem como auxíliar os garçons e a gerencia com gerenciamento de estoque, funcionários, contabilidade, etc.
---
+++ backend/src/router.ts
@@ -0,0 +1,56 @@
+import path from 'node:path';
+import { Router } from 'express';
+import multer from 'multer';
+
+import { listCategories } from './app/useCases/categories/listCategories';
+import { createCategory } from './app/useCases/categories/createCategory';
+import { listProducts } from './app/useCases/products/listProducts';
+import { createProduct } from './app/useCases/products/createProduct';
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { deleteOrder } from './app/useCases/orders/deleteOrder';
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`)
+    }
+  }),
+});
+
+// Categorias
+// Lista todas
+router.get('/categories', listCategories);
+
+// Criar
+router.post('/categories', createCategory)
+
+// Produtos
+// Lista todos
+router.get('/products', listProducts);
+
+// Criar
+router.post('/products', upload.single('image'), createProduct)
+
+// Buscar Produtos por Categoria
+router.get('/categories/:categoryId/products', listProductsByCategory)
+
+// Pedidos
+// Lista
+router.get('/orders', listOrders)
+
+// Criar
+router.post('/orders', createOrder);
+
+// Altera status do pedido
+router.patch('/orders/:orderId', changeOrderStatus)
+
+// Cancela pedido
+router.delete('/orders/:orderId', deleteOrder)
